2bf02a210dac61ce4582f011e6402202880e7ddd
Rogier Wiertz <admin@rogierwiertz.com>
2021-12-10 21:38:09 +0100

    Basic functionality - openTerminals command - killTerminals command - MyTerminals.json settings file

--- extension.js
+++ extension.js
@@ -1,33 +1,69 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
-const vscode = require('vscode');
+const vscode = require('vscode')
 
-// this method is called when your extension is activated
-// your extension is activated the very first time the command is executed
+// Read settings file and parse. Returns settings object or null.
+async function getSettings() {
+	const settingsFile = await vscode.workspace.findFiles('.vscode/**/MyTerminals.json', 'node_modules', 1)
+	.then(files => files?.[0] || null)
+	if (!settingsFile) {
+		return null
+	}
+
+	return vscode.workspace.fs.readFile(settingsFile).then(buffer => {
+		const contents = buffer.toString()
+		try {
+			return JSON.parse(contents)
+		} catch (error) {
+			vscode.window.showErrorMessage('Invalid MyTerminals.json file.')
+			return null
+		}
+	})
+}
+
 
 /**
  * @param {vscode.ExtensionContext} context
  */
-function activate(context) {
+async function activate(context) {
+	let settings = await getSettings()
+	if (!settings) {
+		vscode.window.showWarningMessage('Could not find MyTerminals.json file.', 'Create now', 'Maybe later').then(answer => {
+			if (answer === 'Create now') {
+				console.log('A new file will be created') // @TODO
+			}
+			return
+		})
+	}
+	
+	const openTerminalsCommand = vscode.commands.registerCommand('terminal-vscode-extension.openTerminals', async () => {
+		settings = await getSettings()
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "terminal-vscode-extension" is now active!');
+		if(!settings?.terminals?.length) {
+			return vscode.window.showErrorMessage('No terminals specified in MyTerminals.json file.')
+		}
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with  registerCommand
-	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('terminal-vscode-extension.helloWorld', function () {
-		// The code you place here will be executed every time your command is executed
+		settings.terminals.forEach(t => {
+			const {name, icon, color, message} = t
+			vscode.window.createTerminal({
+				name,
+				iconPath: new vscode.ThemeIcon(icon),
+				color: new vscode.ThemeColor(color),
+				message
+			})
+		})
+		
+		// Focus on terminal
+		const terminalWithFocus = settings.terminals.findIndex(t => t.focus === true)			
+		vscode.window.terminals[terminalWithFocus > -1 ? terminalWithFocus : 0].show()
+	})
 
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from terminal-vscode-extension!');
-	});
-
-	context.subscriptions.push(disposable);
+	const killTerminalsCommand = vscode.commands.registerCommand('terminal-vscode-extension.killTerminals', () => {
+		vscode.window.terminals.forEach(terminal => {
+			terminal.dispose()
+		})
+	})
+	context.subscriptions.push(openTerminalsCommand, killTerminalsCommand)		
 }
 
-// this method is called when your extension is deactivated
 function deactivate() {}
 
 module.exports = {
